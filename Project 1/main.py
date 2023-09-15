6944fd1943893379a1e270abf1063da4db222331
zephyris <fellowtarkovplayer@gmail.com>
2023-09-15 17:40:54 +0800

    3nd commit, everything working now(?), got corre left

--- Project 1/main.py
+++ Project 1/main.py
@@ -1,85 +1,109 @@
+
 def main(csvfile, country: str):
     country = country.lower()
-    index,filter, rowdict = file_io(csvfile, country)
-    sdtv = max_min(filter,rowdict)
-    median, inc_buffer, inc_name = median_salary_SD(filter,index,rowdict)
+    index, filter, row_dict = file_io(csvfile, country)
+    maxmin = max_min(filter, row_dict)
+    median = median_salary_SD(filter, index, row_dict)
+    ratio = profit_ratio(filter, row_dict)
+    corre = correlation(filter, row_dict)
+    return maxmin, median, ratio, corre
 
 
-def file_io(_file: object, _country: object) -> object: #
+# noinspection PyTypeChecker
+def file_io(_file: object, _country: object) -> object:  #
     index = []
     filter = []
-    filedata = open(_file,"r")
+    filedata = open(_file, "r")
     for line in filedata:
-        line = line.lower().split(",")
+        line = line.lower().strip().split(",")  # split,index the file
         index.append(line)
     filedata.close()
-    global rowdict
     rowdict = {}
-    for row in index[0]:
+    for row in index[0]: # index headers
         rowdict.update({row: index[0].index(row)})
     for row in index:
-        if _country in row:
+        if _country in row: # filter by country
             filter.append(row)
-    return index, filter,rowdict
+    return index, filter, rowdict
 
 
-def max_min(_filter: list, _rowdict:dict) -> object:
+def max_min(_filter, _rowdict: dict) -> object:
     max_buffer = 0
     min_buffer = 0
     minmax_output = ["", ""]
-    for row in _filter:
+    for row in _filter: # find max
         if 1981 <= int(row[_rowdict.get("founded")]) <= 2000:
             if max_buffer < float(row[_rowdict.get("number of employees")]):
                 max_buffer = float(row[_rowdict.get("number of employees")])
                 minmax_output[0] = row[_rowdict.get("name")]
+    for row in _filter: # find min
+        if 1981 <= int(row[_rowdict.get("founded")]) <= 2000:
             if min_buffer > float(row[_rowdict.get("number of employees")]) and min != 0:
                 min_buffer = float(row[_rowdict.get("number of employees")])
                 minmax_output[1] = row[_rowdict.get("name")]
-            else:
+            elif min_buffer == 0:
                 min_buffer = float(row[_rowdict.get("number of employees")])
                 minmax_output[1] = row[_rowdict.get("name")]
     return minmax_output
 
 
-def median_salary_SD(_filter,_index:list,_rowdict:dict) -> object:
+def median_salary_SD(_filter, _index: list, _rowdict: dict) -> object:
     # for whole data set
     salary_buffer = []
-    for row in _index[1:]:
-            salary_buffer.append(float(row[_rowdict.get("median salary")]))
+    for row in _index[1:]: # look for median salary based on header index
+        salary_buffer.append(float(row[_rowdict.get("median salary")]))
     mean_all = sum(salary_buffer) / len(salary_buffer)
-    variance = sum([((x - mean_all) ** 2) for x in salary_buffer]) / len(salary_buffer)
-    sd_whole = variance ** 0.5
-    round(sd_whole,4)
-    #for country
+    variance = sum([((x - mean_all) ** 2) for x in salary_buffer]) / (len(salary_buffer) - 1)
+    sd_whole = round(variance ** 0.5, 4)
+    # for country
     salary_buffer.clear()
     for row in _filter:
         salary_buffer.append(int(row[_rowdict.get("median salary")]))
-    mean_country = sum(salary_buffer) / len(salary_buffer)
-    variance = sum([((x - mean_country) ** 2) for x in salary_buffer]) / len(salary_buffer)
-    sd_country = variance ** 0.5
-    round(sd_country,4)
-    return sd_country,sd_whole
+    try:
+        mean_country = sum(salary_buffer) / len(salary_buffer)
+    except ZeroDivisionError: # if no data for country(len(salary_buffer) == 0)
+        return 0
+    variance = sum([((x - mean_country) ** 2) for x in salary_buffer]) / (len(salary_buffer) - 1)
+    sd_country = round(variance ** 0.5, 4)
+    return sd_country, sd_whole
 
-def ratio(_filter:list,_rowdict:dict):
+
+def profit_ratio(_filter: list, _rowdict: dict):
     inc_buffer = []
-    inc_name = []
-    dec_buffer =[]
+    dec_buffer = []
+    for row in _filter: # find profit increase and decrease
+        if float(row[_rowdict.get("profits in 2020(million)")]) > float(row[_rowdict.get("profits in 2021(million)")]):
+            profit_dec = float(row[_rowdict.get("profits in 2020(million)")]) - float(
+                row[_rowdict.get("profits in 2021(million)")])
+            dec_buffer.append(profit_dec) # add to buffer of decreased revenues
     for row in _filter:
-        if row[_rowdict.get("profits in 2020(million)")] > row[_rowdict.get("profit in 2021(million)")]:
-            profit_dec = row[_rowdict.get("profit in 2020(million)")] - row[_rowdict.get("profit in 2021(million)")]
-            dec_buffer.append(profit_dec)
-        else:
-            profit_inc = row[_rowdict.get("profit in 2021(million)")] - row[_rowdict.get("profit in 2020(million)")]
-            inc_buffer.append(profit_inc)
-            inc_name.append(row[_rowdict.get("name")])
-    ratio = sum(inc_buffer) / sum(dec_buffer)
-    return ratio, inc_buffer
+        if float(row[_rowdict.get("profits in 2020(million)")]) < float(row[_rowdict.get("profits in 2021(million)")]):
+            profit_inc = float(row[_rowdict.get("profits in 2021(million)")]) - float(
+                row[_rowdict.get("profits in 2020(million)")])
+            inc_buffer.append(profit_inc) # add to buffer of increased revenues
+    try:
+        ratio = round(sum(inc_buffer) / sum(dec_buffer),4)
+    except ZeroDivisionError:
+        return 0 # if no data for country(len(salary_buffer) == 0)
+    return ratio
 
 
-def correlation(_filter,_inc_buffer,_inc_name:list,_rowdict:dict):
-    for row in _filter:
-        if row[_rowdict.get("name")] in _inc_name:
-            correlation.buffer[]
-
-    pass
-main("Organisations.csv", "Belgium")
+def correlation(_filter: list, _rowdict: dict):
+    profit_list = []
+    median_list = []
+    for row in _filter: # create index of increased revenues & median salaries
+        if float(row[_rowdict.get("profits in 2021(million)")]) > float(row[_rowdict.get("profits in 2020(million)")]):
+            profit_list.append(float(row[_rowdict.get("profits in 2021(million)")]))
+            median_list.append(float(row[_rowdict.get("median salary")]))
+    try:
+        average_profit = sum(profit_list) / len(profit_list)
+    except ZeroDivisionError:
+        return 0 # if no data for country(len(salary_buffer) == 0)
+    average_median = sum(median_list) / len(median_list)
+    buffer1, buffer2, buffer3 = 0, 0, 0
+    for _ in range(len(profit_list)):
+        buffer1 += (profit_list[_] - average_profit) * (median_list[_] - average_median)
+        buffer2 += (profit_list[_] - average_profit) ** 2
+        buffer3 += (median_list[_] - average_median) ** 2
+    cor = round(buffer1 / ((buffer2 *buffer3) ** 0.5),4) # calculate correlation
+    return cor
